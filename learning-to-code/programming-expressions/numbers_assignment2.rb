ea9c81b88b5bc62c6635077306a28396ec48cc43
saintmedusa <54053695+saintmedusa@users.noreply.github.com>
2019-10-20 23:22:29 -0700

    Upload numbers assignment v2

---
+++ learning-to-code/programming-expressions/numbers_assignment2.rb
@@ -0,0 +1,33 @@
+# This program asks for three integers, 
+# and returns a evaluation on the integers comparison to 20
+puts "Let's see how three integers compare to 20"
+
+# gather integer 1
+puts " Enter the 1st number:"
+num1 = gets.to_i
+# evaluate integer 1
+if num1 < 20
+    puts "#{num1} is less than 20."
+else 
+    puts "#{num1} is greater than or equal to 20"
+end
+
+# gather integer 2
+puts " Enter the 2nd number:"
+num2 = gets.to_i
+# evaluate integer 2
+if num1 < 20
+    puts "#{num2} is less than 20."
+else 
+    puts "#{num2} is greater than or equal to 20"
+end
+
+# gather integer 3
+puts " Enter the 3rd number:"
+num3 = gets.to_i
+# evaluate integer 3
+if num1 < 20
+    puts "#{num3} is less than 20."
+else 
+    puts "#{num3} is greater than or equal to 20"
+end
